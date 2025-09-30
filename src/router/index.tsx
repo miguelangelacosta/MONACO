dcef50173d60df96a858fb4eb66dab8d096123f9
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2025-09-30 11:13:48 -0500

    cambios para redireccion de enlaces01

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,67 +1,110 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { RootLayout } from '../layouts/RootLayout';
 import {
-  HomePage,
-  CellPhonesPage,
-  AboutPage,
-  CellPhonePage,
-  LoginPage,
-  RegisterPage,
-  OrdersUserPage,
-  CheckoutPage,
-  ThankyouPage,
-  OrderUserPage,
-  DashboardProductsPage,
-  DashboardNewProductPage,
-  DashboardProductSlugPage,
-  DashboardOrdersPage,
-  DashboardOrderPage,
+	HomePage,
+	CellPhonesPage,
+	AboutPage,
+	CellPhonePage,
+	LoginPage,
+	RegisterPage,
+	OrdersUserPage,
+	CheckoutPage,
+	ThankyouPage,
+	OrderUserPage,
+	DashboardProductsPage,
+	DashboardNewProductPage,
+	DashboardProductSlugPage,
+	DashboardOrdersPage,
+	DashboardOrderPage,
 } from '../pages';
 import { ClientLayout } from '../layouts/ClientLayout';
 import { DashboardLayout } from '../layouts/DashboardLayout';
 
-// Maneja errores en el dashboard
-const DashboardError = () => (
-  <div className="flex items-center justify-center h-screen">
-    <h1 className="text-2xl font-bold">Oops! Algo salió mal en el dashboard 😅</h1>
-  </div>
-);
-
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: 'tienda', element: <CellPhonesPage /> },
-      { path: 'celulares/:slug', element: <CellPhonePage /> },
-      { path: 'nosotros', element: <AboutPage /> },
-      { path: 'login', element: <LoginPage /> },
-      { path: 'registro', element: <RegisterPage /> },
-      {
-        path: 'account',
-        element: <ClientLayout />,
-        children: [
-          { path: '', element: <Navigate to="/account/pedidos" /> },
-          { path: 'pedidos', element: <OrdersUserPage /> },
-          { path: 'pedidos/:id', element: <OrderUserPage /> },
-        ],
-      },
-    ],
-  },
-  { path: '/checkout', element: <CheckoutPage /> },
-  { path: '/checkout/:id/thank-you', element: <ThankyouPage /> },
-  {
-    path: '/dashboard',
-    element: <DashboardLayout />,
-    errorElement: <DashboardError />, // captura errores del dashboard
-    children: [
-      { index: true, element: <Navigate to="/dashboard/productos" /> },
-      { path: 'productos', element: <DashboardProductsPage /> },
-      { path: 'productos/new', element: <DashboardNewProductPage /> },
-      { path: 'productos/editar/:slug', element: <DashboardProductSlugPage /> },
-      { path: 'ordenes', element: <DashboardOrdersPage /> },
-      { path: 'ordenes/:id', element: <DashboardOrderPage /> },
-    ],
-  },
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			{
+				path: 'tienda',
+				element: <CellPhonesPage />,
+			},
+			{
+				path: 'celulares/:slug',
+				element: <CellPhonePage />,
+			},
+			{
+				path: 'nosotros',
+				element: <AboutPage />,
+			},
+			{
+				path: 'login',
+				element: <LoginPage />,
+			},
+			{
+				path: 'registro',
+				element: <RegisterPage />,
+			},
+			{
+				path: 'account',
+				element: <ClientLayout />,
+				children: [
+					{
+						path: '',
+						element: <Navigate to='/account/pedidos' />,
+					},
+					{
+						path: 'pedidos',
+						element: <OrdersUserPage />,
+					},
+					{
+						path: 'pedidos/:id',
+						element: <OrderUserPage />,
+					},
+				],
+			},
+		],
+	},
+	{
+		path: '/checkout',
+		element: <CheckoutPage />,
+	},
+	{
+		path: '/checkout/:id/thank-you',
+		element: <ThankyouPage />,
+	},
+	{
+		path: '/dashboard',
+		element: <DashboardLayout />,
+		children: [
+			{
+				index: true,
+				element: <Navigate to='/dashboard/productos' />,
+			},
+			{
+				path: 'productos',
+				element: <DashboardProductsPage />,
+			},
+			{
+				path: 'productos/new',
+				element: <DashboardNewProductPage />,
+			},
+			{
+				path: 'productos/editar/:slug',
+				element: <DashboardProductSlugPage />,
+			},
+			{
+				path: 'ordenes',
+				element: <DashboardOrdersPage />,
+			},
+			{
+				path: 'ordenes/:id',
+				element: <DashboardOrderPage />,
+			},
+		],
+	},
 ]);
